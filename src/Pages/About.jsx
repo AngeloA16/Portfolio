dd22f5f1c2fafc9830ed0d3c21fb9d47450fe869
AngeloA16 <Angeloadvincula0@gmail.com>
2026-09-03 18:13:44 +0800

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -125,7 +125,7 @@ const AboutPage = () => {
       const storedProjects = JSON.parse(localStorage.getItem("projects") || "[]");
       const storedCertificates = JSON.parse(localStorage.getItem("certificates") || "[]");
       
-      const startDate = new Date("2024-11-11");
+      const startDate = new Date("2023-11-11");
       const today = new Date();
       const experience = today.getFullYear() - startDate.getFullYear() -
         (today < new Date(today.getFullYear(), startDate.getMonth(), startDate.getDate()) ? 1 : 0);
